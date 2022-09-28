6346e21499bf2a9576b7e46e5315ebefbfc75711
David Burke <davidburke@shannontechnicalservices.com>
2022-09-28 12:54:53 +0100

    Rewrote with functions

--- LDND-Format.py
+++ LDND-Format.py
@@ -9,6 +9,28 @@ Str = pd.read_excel('data\mpdsup.xls', "STRUCTURAL MAINTENANCE PROGRAM", skiprow
 Zon = pd.read_excel('data\mpdsup.xls', "ZONAL INSPECTION PROGRAM", skiprows=5, names=["Revision", "Index", "MPD ITEM NUMBER", "AMM REFERENCE", "ZONE", "ACCESS", "INTERVAL THRES.", "INTERVAL REPEAT", "APPLICABILITY APL", "APPLICABILITY ENG", "MAN-HOURS", "TASK DESCRIPTION"])
 
 ############################ Functions ############################
+def daysSorter(mpd, compilecheck, columnFrom, columnTo, factor):
+    for index, row in mpd.iterrows():
+        # On each row check the Taks Description for a match with Airplane Note
+        pattern = re.compile(compilecheck)
+        result = re.search(pattern, str(row[columnFrom]))
+        # if the result is not None
+        if result != None:
+            days = int(result.group(0))*factor
+            # Print the extracted text to the new column
+            mpd.loc[index, columnTo] = days
+
+def extracter(mpd,extraction,columnFrom,columnTo):
+        for index, row in mpd.iterrows():
+        # On each row check the Taks Description for a match with Airplane Note
+            pattern = re.compile(extraction)
+            result = re.search(pattern, str(row[columnFrom]))
+            # if the result is not None
+            if result != None:
+                # Print the extracted text to the new column
+                mpd.loc[index,columnTo] = int(result.group(0))
+
+############################ Functions ############################
 def formatter(mpd, path):
     # Inserting a blank row for the extracted notes
     colNames = ('THRES. Flight Hours', 'THRES. Flight Cycles', 'THRES. Calendar Days', 'REPEAT Flight Hours', 'REPEAT Flight Cycles', 'REPEAT Calendar Days')
@@ -16,140 +38,21 @@ def formatter(mpd, path):
     for name in colNames:
         mpd.insert(column = name, loc = int(len(mpd.columns)), value = np.nan)
 
-    for index, row in mpd.iterrows():
-        # On each row check the Taks Description for a match with Airplane Note
-        pattern = re.compile(r'\d+(?= FH| AH)')
-        result = re.search(pattern, str(row['INTERVAL THRES.']))
-        # if the result is not None
-        if result != None:
-            # Print the extracted text to the new column
-            mpd.loc[index, 'THRES. Flight Hours'] = int(result.group(0))
+    extracter(mpd,r'\d+(?= FH| AH)','INTERVAL THRES.','THRES. Flight Hours')
+    extracter(mpd,r'\d+(?= FC)','INTERVAL THRES.','THRES. Flight Cycles')
+    extracter(mpd,r'\d+(?= FH| AH)','INTERVAL REPEAT','REPEAT Flight Hours')
+    extracter(mpd,r'\d+(?= FC)','INTERVAL REPEAT','REPEAT Flight Cycles')
+    
+    daysSorter(mpd,r'\d+(?= HR)','INTERVAL THRES.','THRES. Calendar Days',(1/24))
+    daysSorter(mpd,r'\d+(?= HR)','INTERVAL REPEAT','REPEAT Calendar Days',(1/24))
+    daysSorter(mpd,r'\d+(?= MO)','INTERVAL THRES.','THRES. Calendar Days',30.437)
+    daysSorter(mpd,r'\d+(?= MO)','INTERVAL REPEAT','REPEAT Calendar Days',30.437)
+    daysSorter(mpd,r'\d+(?= YR)','INTERVAL THRES.','THRES. Calendar Days',365.25)
+    daysSorter(mpd,r'\d+(?= YR)','INTERVAL REPEAT','REPEAT Calendar Days',365.25)
+    daysSorter(mpd,r'\d+(?= DY)','INTERVAL THRES.','THRES. Calendar Days',1)
+    daysSorter(mpd,r'\d+(?= DY)','INTERVAL REPEAT','REPEAT Calendar Days',1)
 
-    for index, row in mpd.iterrows():
-        # On each row check the Taks Description for a match with Airplane Note
-        pattern = re.compile(r'\d+(?= FC)')
-        result = re.search(pattern, str(row['INTERVAL THRES.']))
-        # if the result is not None
-        if result != None:
-            # Print the extracted text to the new column
-            mpd.loc[index, 'THRES. Flight Cycles'] = int(result.group(0))
-
-    for index, row in mpd.iterrows():
-        # On each row check the Taks Description for a match with Airplane Note
-        pattern = re.compile(r'\d+ (HR|DY|MO|YR)')
-        result = re.search(pattern, str(row['INTERVAL THRES.']))
-        # if the result is not None
-        if result != None:
-            # Print the extracted text to the new column
-            mpd.loc[index, 'THRES. Calendar Days'] = result.group(0)
-
-    for index, row in mpd.iterrows():
-        # On each row check the Taks Description for a match with Airplane Note
-        pattern = re.compile(r'\d+(?= FH| AH)')
-        result = re.search(pattern, str(row['INTERVAL REPEAT']))
-        # if the result is not None
-        if result != None:
-            # Print the extracted text to the new column
-            mpd.loc[index, 'REPEAT Flight Hours'] = int(result.group(0))
-
-    for index, row in mpd.iterrows():
-        # On each row check the Taks Description for a match with Airplane Note
-        pattern = re.compile(r'\d+(?= FC)')
-        result = re.search(pattern, str(row['INTERVAL REPEAT']))
-        # if the result is not None
-        if result != None:
-            # Print the extracted text to the new column
-            mpd.loc[index, 'REPEAT Flight Cycles'] = int(result.group(0))
-
-    for index, row in mpd.iterrows():
-        # On each row check the Taks Description for a match with Airplane Note
-        pattern = re.compile(r'\d+ (HR|DY|MO|YR)')
-        result = re.search(pattern, str(row['INTERVAL REPEAT']))
-        # if the result is not None
-        if result != None:
-            # Print the extracted text to the new column
-            mpd.loc[index, 'REPEAT Calendar Days'] = result.group(0)
-
-    for index, row in mpd.iterrows():
-        # On each row check the Taks Description for a match with Airplane Note
-        pattern = re.compile(r'\d+(?= HR)')
-        result = re.search(pattern, str(row['THRES. Calendar Days']))
-        # if the result is not None
-        if result != None:
-            days = int(result.group(0))/24
-            # Print the extracted text to the new column
-            mpd.loc[index, 'THRES. Calendar Days'] = days
-
-    for index, row in mpd.iterrows():
-        # On each row check the Taks Description for a match with Airplane Note
-        pattern = re.compile(r'\d+(?= HR)')
-        result = re.search(pattern, str(row['REPEAT Calendar Days']))
-        # if the result is not None
-        if result != None:
-            days = int(result.group(0))/24
-            # Print the extracted text to the new column
-            mpd.loc[index, 'REPEAT Calendar Days'] = days
-
-    for index, row in mpd.iterrows():
-        # On each row check the Taks Description for a match with Airplane Note
-        pattern = re.compile(r'\d+(?= MO)')
-        result = re.search(pattern, str(row['THRES. Calendar Days']))
-        # if the result is not None
-        if result != None:
-            days = int(result.group(0))*30.437
-            # Print the extracted text to the new column
-            mpd.loc[index, 'THRES. Calendar Days'] = days
-
-    for index, row in mpd.iterrows():
-        # On each row check the Taks Description for a match with Airplane Note
-        pattern = re.compile(r'\d+(?= MO)')
-        result = re.search(pattern, str(row['REPEAT Calendar Days']))
-        # if the result is not None
-        if result != None:
-            days = int(result.group(0))*30.437
-            # Print the extracted text to the new column
-            mpd.loc[index, 'REPEAT Calendar Days'] = days
-
-    for index, row in mpd.iterrows():
-        # On each row check the Taks Description for a match with Airplane Note
-        pattern = re.compile(r'\d+(?= YR)')
-        result = re.search(pattern, str(row['THRES. Calendar Days']))
-        # if the result is not None
-        if result != None:
-            days = int(result.group(0))*365.25
-            # Print the extracted text to the new column
-            mpd.loc[index, 'THRES. Calendar Days'] = days
-
-    for index, row in mpd.iterrows():
-        # On each row check the Taks Description for a match with Airplane Note
-        pattern = re.compile(r'\d+(?= YR)')
-        result = re.search(pattern, str(row['REPEAT Calendar Days']))
-        # if the result is not None
-        if result != None:
-            days = int(result.group(0))*365.25
-            # Print the extracted text to the new column
-            mpd.loc[index, 'REPEAT Calendar Days'] = days
-
-    for index, row in mpd.iterrows():
-        # On each row check the Taks Description for a match with Airplane Note
-        pattern = re.compile(r'\d+(?= DY)')
-        result = re.search(pattern, str(row['THRES. Calendar Days']))
-        # if the result is not None
-        if result != None:
-            days = int(result.group(0))
-            # Print the extracted text to the new column
-            mpd.loc[index, 'THRES. Calendar Days'] = days
-
-    for index, row in mpd.iterrows():
-        # On each row check the Taks Description for a match with Airplane Note
-        pattern = re.compile(r'\d+(?= DY)')
-        result = re.search(pattern, str(row['REPEAT Calendar Days']))
-        # if the result is not None
-        if result != None:
-            days = int(result.group(0))
-            # Print the extracted text to the new column
-            mpd.loc[index, 'REPEAT Calendar Days'] = days
+    # Print to Excel
     mpd.to_excel(path)
-
 
 formatter(SnP, "SnP.xlsx")
