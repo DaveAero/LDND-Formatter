fd72bcefb990f2c62006719f5d185c55fe4128a8
David Burke <davidgerburke@gmail.com>
2022-09-28 21:08:42 +0100

    tidy up of code and added LLP/Hard Time function

--- LDND-Format.py
+++ LDND-Format.py
@@ -9,7 +9,7 @@ Str = pd.read_excel('data\mpdsup.xls', "STRUCTURAL MAINTENANCE PROGRAM", skiprow
 Zon = pd.read_excel('data\mpdsup.xls', "ZONAL INSPECTION PROGRAM", skiprows=5, names=["Revision", "Index", "MPD ITEM NUMBER", "AMM REFERENCE", "ZONE", "ACCESS", "INTERVAL THRES.", "INTERVAL REPEAT", "APPLICABILITY APL", "APPLICABILITY ENG", "MAN-HOURS", "TASK DESCRIPTION"])
 
 ############################ Functions ############################
-def daysSorter(mpd, compilecheck, columnFrom, columnTo, factor):
+def extracter(mpd, compilecheck, columnFrom, columnTo, factor):
     for index, row in mpd.iterrows():
         # On each row check the Taks Description for a match with Airplane Note
         pattern = re.compile(compilecheck)
@@ -19,16 +19,19 @@ def daysSorter(mpd, compilecheck, columnFrom, columnTo, factor):
             days = int(result.group(0))*factor
             # Print the extracted text to the new column
             mpd.loc[index, columnTo] = days
+        
+        # Sorting out string entries in intervals that will remain the same
+        if str(row[columnFrom]) == 'LIF LIM':
+            mpd.loc[index,columnTo] = str('LLP')
+        
+        if str(row[columnFrom]) == 'APU CNG':
+            mpd.loc[index,columnTo] = str('APU CNG')
 
-def extracter(mpd,extraction,columnFrom,columnTo):
-        for index, row in mpd.iterrows():
-        # On each row check the Taks Description for a match with Airplane Note
-            pattern = re.compile(extraction)
-            result = re.search(pattern, str(row[columnFrom]))
-            # if the result is not None
-            if result != None:
-                # Print the extracted text to the new column
-                mpd.loc[index,columnTo] = int(result.group(0))
+        if str(row[columnFrom]) == 'ENG CNG':
+            mpd.loc[index,columnTo] = str('ENG CNG')
+
+        if str(row[columnFrom]) == 'VEN REC':
+            mpd.loc[index,columnTo] = str('VEN REC')
 
 ############################ Functions ############################
 def formatter(mpd, path):
@@ -38,21 +41,24 @@ def formatter(mpd, path):
     for name in colNames:
         mpd.insert(column = name, loc = int(len(mpd.columns)), value = np.nan)
 
-    extracter(mpd,r'\d+(?= FH| AH)','INTERVAL THRES.','THRES. Flight Hours')
-    extracter(mpd,r'\d+(?= FC)','INTERVAL THRES.','THRES. Flight Cycles')
-    extracter(mpd,r'\d+(?= FH| AH)','INTERVAL REPEAT','REPEAT Flight Hours')
-    extracter(mpd,r'\d+(?= FC)','INTERVAL REPEAT','REPEAT Flight Cycles')
+    extracter(mpd,r'\d+(?= FH| AH)','INTERVAL THRES.','THRES. Flight Hours',1)
+    extracter(mpd,r'\d+(?= FC)','INTERVAL THRES.','THRES. Flight Cycles',1)
+    extracter(mpd,r'\d+(?= FH| AH)','INTERVAL REPEAT','REPEAT Flight Hours',1)
+    extracter(mpd,r'\d+(?= FC)','INTERVAL REPEAT','REPEAT Flight Cycles',1)
     
-    daysSorter(mpd,r'\d+(?= HR)','INTERVAL THRES.','THRES. Calendar Days',(1/24))
-    daysSorter(mpd,r'\d+(?= HR)','INTERVAL REPEAT','REPEAT Calendar Days',(1/24))
-    daysSorter(mpd,r'\d+(?= MO)','INTERVAL THRES.','THRES. Calendar Days',30.437)
-    daysSorter(mpd,r'\d+(?= MO)','INTERVAL REPEAT','REPEAT Calendar Days',30.437)
-    daysSorter(mpd,r'\d+(?= YR)','INTERVAL THRES.','THRES. Calendar Days',365.25)
-    daysSorter(mpd,r'\d+(?= YR)','INTERVAL REPEAT','REPEAT Calendar Days',365.25)
-    daysSorter(mpd,r'\d+(?= DY)','INTERVAL THRES.','THRES. Calendar Days',1)
-    daysSorter(mpd,r'\d+(?= DY)','INTERVAL REPEAT','REPEAT Calendar Days',1)
+    extracter(mpd,r'\d+(?= HR)','INTERVAL THRES.','THRES. Calendar Days',(1/24))
+    extracter(mpd,r'\d+(?= HR)','INTERVAL REPEAT','REPEAT Calendar Days',(1/24))
+    extracter(mpd,r'\d+(?= MO)','INTERVAL THRES.','THRES. Calendar Days',30.437)
+    extracter(mpd,r'\d+(?= MO)','INTERVAL REPEAT','REPEAT Calendar Days',30.437)
+    extracter(mpd,r'\d+(?= YR)','INTERVAL THRES.','THRES. Calendar Days',365.25)
+    extracter(mpd,r'\d+(?= YR)','INTERVAL REPEAT','REPEAT Calendar Days',365.25)
+    extracter(mpd,r'\d+(?= DY)','INTERVAL THRES.','THRES. Calendar Days',1)
+    extracter(mpd,r'\d+(?= DY)','INTERVAL REPEAT','REPEAT Calendar Days',1)
 
     # Print to Excel
     mpd.to_excel(path)
 
+############################ Main Code ############################
 formatter(SnP, "SnP.xlsx")
+formatter(Str, "Str.xlsx")
+formatter(Zon, "Zon.xlsx")
